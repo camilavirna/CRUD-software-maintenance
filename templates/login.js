5e7061aed8f89fadd0f7419dffaee2b8dd93927f
jonas <jonascaetanodesouza@gmail.com>
2022-06-09 22:03:17 -0300

    first commit

---
+++ templates/login.js
@@ -0,0 +1,39 @@
+const urlParams = new URLSearchParams(window.location.search);
+const erro = urlParams.get('erro');
+
+document.querySelector("body > div.main > div.login > form > label").click()
+
+if(erro == '0'){
+
+	erromsg = 'usuario não registrado no sistema.'
+	document.querySelector('.erro').textContent = erromsg
+	document.querySelector("label").click()
+} 
+
+
+else if(erro == '1'){
+
+erromsg = 'a senha está incorreta.'
+document.querySelector('.erro').textContent = erromsg
+}
+
+else if (erro == '2'){
+
+	erromsg = 'email já esta cadastado, se esqueceu a senha entre em contato.'
+	document.querySelector('.erro').textContent = erromsg
+}
+
+else if (erro == '3'){
+
+	erromsg = 'usuario cadastado com sucesso, faça login novamente!'
+	document.querySelector('.erro').textContent = erromsg
+	document.querySelector('.erro').style.color = 'green'
+
+}
+
+else if (erro == '4'){
+
+	erromsg = 'sua sessão expirou, faça login novamente.'
+	document.querySelector('.erro').textContent = erromsg
+
+}
